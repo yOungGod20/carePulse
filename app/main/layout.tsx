1ecc610f1a4e8a7a6a93cdcd6ca2af330e7d5f54
Yang <1746519797@qq.com>
2024-08-22 19:16:11 +0800

    fix

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await auth();
+  let session;
+  while (!session) {
+    session = await auth();
+    console.log(111);
+  }
   return (
     <SessionProvider session={session}>
       <div className="mx-auto max-w-[1480px] flex flex-col w-full h-screen">
